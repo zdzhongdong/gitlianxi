362b7c32c3ecab93dada4b516f315a13a3b8437a
zd <1453033913@qq.com>
2022-04-14 17:22:40 +0800

    初始提交

--- src/main/java/test02.java
+++ src/main/java/test02.java
@@ -1,5 +1,8 @@
 public class test02 {
     public static void main(String[] args) {
         System.out.println("test02...");
+        System.out.println("111...");
+        System.out.println("222...");
+        System.out.println("3333...");
     }
 }
